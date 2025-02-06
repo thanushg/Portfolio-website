e50eafe0092273f46f68f461371a065c4ba9809a
thanushg <luckythanush143@gmail.com>
2025-02-06 08:59:02 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll(".navbar a").forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+})
+function viewResume(){
+    window.open("https://drive.google.com/file/d/1reVsoIRnn_kmno4O3K6gZhDgHTJmm8DT/view?usp=sharing/browser","_blank")
+}
+function Aws(){
+    window.open("https://drive.google.com/file/d/1drC-IM19zyir6IZtUp-qZdMn1GxQmk86/view?usp=sharing","_blank")
+}
+function Ai(){
+    window.open("https://drive.google.com/file/d/1oytTqmjXTZJ0TcDjVBoNU23Z9Tx3Cyq9/view?usp=sharing","_blank")
+}
+function python(){
+    window.open("https://drive.google.com/file/d/11_2nGsH1tF_4MKQZaQcDXSxLiOk0sa91/view?usp=sharing","_blank")
+}
